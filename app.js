661342f7ef948afff7dd41d3b72b6e54ea4e7849
Fabio Casati <fabio.casati@gmail.com>
2018-01-24 20:23:52 +0100

    add basic structure to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const express = require('express'),
+    bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.set('port', (process.env.PORT || 5000));
+
+// a useless function that returns a fixed object. you can use it, if you want, for testing purposes
+app.get('/count',function (req, res) {
+    res.json({count: 5})
+})
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
